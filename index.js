f4f4f0e09c868b8266936eff1e0b37778ec5615e
Christine Patton <christine@pattoncreative.com>
2018-07-24 19:56:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution here!
+const drivers=['Milo', 'Otis', 'Garfield'];
+console.log(drivers);
+
+function destructivelyAppendDriver(name) {
+  drivers.push('Ralph')
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift('Bob')
+}
+
+function destructivelyRemoveLastDriver(name) {
+  drivers.pop()
+}
+
+
+
